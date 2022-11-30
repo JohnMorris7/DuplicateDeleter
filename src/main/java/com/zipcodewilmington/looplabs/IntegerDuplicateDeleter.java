5874bfb953627da3b44d3901958486cf4c1b24a5
John <john@Zipcoders-MacBook-Pro-46.local>
2022-11-30 17:22:51 -0500

    finished

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -5,4 +5,85 @@ package com.zipcodewilmington.looplabs;
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+
+    Integer[] intarray;
+
+
+
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+        this.intarray = intArray;
+    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+
+        int counterTwo = 0;//creating 2nd counter
+        for (int i = 0; i < intarray.length; i++) {//for loop to count through
+            int counter = 0;//creating 1 counter
+            for (int j = 0; j < intarray.length; j++) { //2nd for loop to count how many duplicates are
+                if(intarray[j] == intarray[i]) {//if intarray j while iterating through finds a duplicate its holding on to it
+                    counter++;
+                }
+
+                //
+            }if(counter < maxNumberOfDuplications) {
+                counterTwo++;
+            }
+
+        }
+        Integer[] result = new Integer[counterTwo];
+        int counterThree = 0;
+        for (int i = 0; i < intarray.length; i++) {//for loop to count through
+            int counter = 0;
+            for (int j = 0; j < intarray.length; j++) { //2nd for loop to count how many duplicates are
+                if(intarray[j] == intarray[i]) {//if intarray j while iterating through finds a duplicate its holding on to it
+                    counter++;
+                }
+
+            }if(counter < maxNumberOfDuplications) {
+                result[counterThree]  = intarray[i];
+                counterThree++;
+            }
+
+        }
+
+        return result;
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+
+        int counterTwo = 0;//creating 2nd counter
+        for (int i = 0; i < intarray.length; i++) {//for loop to count through
+            int counter = 0;//creating 1 counter
+            for (int j = 0; j < intarray.length; j++) { //2nd for loop to count how many duplicates are
+                if(intarray[j] == intarray[i]) {//if intarray j while iterating through finds a duplicate its holding on to it
+                    counter++;
+                }
+
+               //
+            }if(counter != exactNumberOfDuplications) {
+                counterTwo++;
+            }
+
+        }
+        Integer[] result = new Integer[counterTwo];
+        int counterThree = 0;
+        for (int i = 0; i < intarray.length; i++) {//for loop to count through
+            int counter = 0;
+            for (int j = 0; j < intarray.length; j++) { //2nd for loop to count how many duplicates are
+                if(intarray[j] == intarray[i]) {//if intarray j while iterating through finds a duplicate its holding on to it
+                    counter++;
+                }
+
+            }if(counter != exactNumberOfDuplications) {
+                result[counterThree]  = intarray[i];
+                counterThree++;
+            }
+
+        }
+
+        return result;
+    }
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,8 +1,87 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Objects;
+
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+
+    String[] strArray;
+
+    public StringDuplicateDeleter(String[] intArray) {
+        super(intArray);
+        this.strArray = intArray;
+    }
+
+    @Override
+    public String[] removeDuplicates(int maxNumberOfDuplications) {
+        int counterTwo = 0;//creating 2nd counter
+        for (int i = 0; i < strArray.length; i++) {//for loop to count through
+            int counter = 0;//creating 1 counter
+            for (int j = 0; j < strArray.length; j++) { //2nd for loop to count how many duplicates there are
+                if(Objects.equals(strArray[j], strArray[i])) {//if intarray j while iterating through finds a duplicate its holding on to it
+                    counter++;
+                }
+
+
+            }if(counter < maxNumberOfDuplications) {
+                counterTwo++;
+            }
+
+        }
+        String[] result = new String[counterTwo];
+        int counterThree = 0;
+        for (int i = 0; i < strArray.length; i++) {//for loop to count through
+            int counter = 0;
+            for (int j = 0; j < strArray.length; j++) { //2nd for loop to count how many duplicates are
+                if(Objects.equals(strArray[j], strArray[i])) {//if intarray j while iterating through finds a duplicate its holding on to it
+                    counter++;
+                }
+
+            }if(counter < maxNumberOfDuplications) {
+                result[counterThree]  = strArray[i];
+                counterThree++;
+            }
+
+        }
+
+        return result;
+    }
+
+    @Override
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        int counterTwo = 0;//creating 2nd counter
+        for (int i = 0; i < strArray.length; i++) {//for loop to count through
+            int counter = 0;//creating 1 counter
+            for (int j = 0; j < strArray.length; j++) { //2nd for loop to count how many duplicates there are
+                if(Objects.equals(strArray[j], strArray[i])) {//if intarray j while iterating through finds a duplicate its holding on to it
+                    counter++;
+                }
+
+
+            }if(counter != exactNumberOfDuplications) {
+                counterTwo++;
+            }
+
+        }
+        String[] result = new String[counterTwo];
+        int counterThree = 0;
+        for (int i = 0; i < strArray.length; i++) {//for loop to count through
+            int counter = 0;
+            for (int j = 0; j < strArray.length; j++) { //2nd for loop to count how many duplicates are
+                if(Objects.equals(strArray[j], strArray[i])) {//if intarray j while iterating through finds a duplicate its holding on to it
+                    counter++;
+                }
+
+            }if(counter != exactNumberOfDuplications) {
+                result[counterThree]  = strArray[i];
+                counterThree++;
+            }
+
+        }
+
+        return result;
+    }
 }
